d6ab9b3883118886121825fd9fa25beb9d1eed64
LOCOS DEVELOPER <jmn103873@gmail.com>
2026-07-28 11:49:01 -0700

    Update categories.tsx

--- unitech/src/pages/categories.tsx
+++ unitech/src/pages/categories.tsx
@@ -5,7 +5,7 @@ import {
   useUpdateCategory,
   useDeleteCategory,
   Category
-} from "@workspace/api-client-react";
+} from "@/lib/api";
 import { useQueryClient } from "@tanstack/react-query";
 import { Layout } from "@/components/layout";
 import { Button } from "@/components/ui/button";
